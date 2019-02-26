e866ffb0ee87e5e90807f1786a21ffeeb41dd8db
Paul <paul.leitner89@gmail.com>
2019-02-26 13:01:48 +0100

    added file to remove previous output

---
+++ cleanup.py
@@ -0,0 +1,10 @@
+import os
+
+
+to_delete = list(filter(lambda x: x.startswith('auto_output'), os.listdir()))
+choice = input('delete all auto_output files? YES/n: ')
+
+if choice == 'YES':
+    [os.remove(i) for i in to_delete]
+else:
+    pass
