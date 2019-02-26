94dfcefbfa4dc123872978b1c67a0bb8ee155137
Paul <paul.leitner89@gmail.com>
2019-02-26 09:07:12 +0100

    added docstring - fin

--- weekday_count.py
+++ weekday_count.py
@@ -6,7 +6,9 @@ t_end_date = dt.date(2018, 12, 31)
 
 
 def weekday_count(*, start_date, end_date):
-    """docstring sample"""
+    """this function counts all weekdays between a start and an end date
+    needs start_date and end_date as datetime.date objects
+    :returns dict - keys 0-6 - 0 = Monday - values = int amount of occurrences between start-end"""
     day_range = [start_date + dt.timedelta(days=i) for i in range((end_date - start_date).days + 1)]
     wdays = ['mo', 'di', 'mi', 'do', 'fr', 'sa', 'so']
     amount_days = {k: len(list(filter(lambda x: x.weekday() == k, day_range))) for k, v in enumerate(wdays)}
